47ece0ab32cb6cd89a79596b943ffdfd2acfb8ec
soy-bee <kitatlasiden@gmail.com>
2021-03-25 16:17:45 +1100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace program 
+{
+    class Program{
+        static void Main(string[] args) {
+
+           
+
+            System.Console.WriteLine("How many times would you like your name repeated?");
+            string input2 = Console.ReadLine();
+            int repeatNum = int.Parse(input2);
+
+
+            int counter = 0;
+
+            while ( counter  <=repeatNum)
+            {
+                System.Console.Write("Please enter a name");
+            string input = Console.ReadLine();
+
+               //counter = counter + 1
+               counter++; 
+            }
+
+            System.Console.WriteLine("Goodbye");
+        }
+    }
+}
